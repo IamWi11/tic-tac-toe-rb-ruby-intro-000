64079b6ce9a5ee7ff7d01ea5311f96d7c6ec64af
William <william.l.munoz@gmail.com>
2020-04-22 14:37:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,9 +32,9 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if !valid_move?(board, index)
-  turn(board)
+    turn(board)
   else
-  move(board, index, current_player(board))
+    move(board, index, current_player(board))
   end
   display(board)
 end
